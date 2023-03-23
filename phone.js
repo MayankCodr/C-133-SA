2a9341fe687ea3c606e8d03bf6d91d4ab6145f27
MayankCodr <96002289+MayankCodr@users.noreply.github.com>
2023-03-23 16:07:21 +0530

    Add files via upload

---
+++ phone.js
@@ -0,0 +1,47 @@
+img = "";
+status = "";
+objects = [];
+function preload(){
+    img = loadImage("laptop_phone1.jpg");
+
+}
+function setup(){
+    canvas = createCanvas(640, 400);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status :- Detecting Object";
+
+
+}
+function modelLoaded(){
+    console.log("Model is loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+
+}
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+function draw(){
+    image(img, 0, 0, 640, 400);
+    if (status != "") {
+        for ( i = 0; i < objects.length; i++) {
+           document.getElementById("status").innerHTML = "Status :  Object Detected";
+           fill("red");
+           percent = floor(objects[i].confidence*100);
+           text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y +15);
+           noFill();
+           stroke("red");
+           rect(objects[i].x, objects[i].y , objects[i].width, objects[i].height);
+
+        }
+   }
+
+
+}
